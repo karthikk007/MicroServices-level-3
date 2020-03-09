056043d8097a898f52025c30ec87963df67dfbc5
KARTHIK KUMAR <karthik.kumar@rakuten.com>
2020-03-09 13:27:08 +0530

    print environment details

--- spring-boot-config/src/main/java/com/kk/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/kk/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package com.kk.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,10 +36,18 @@ public class GreetingController {
     @Autowired
     private DBSettings dbSettings;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/greeting")
     public String getGreeting() {
 //        return staticMessage + defaultValue + listValues + dbValues;
         return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
+    }
+
+    @GetMapping("/envdetails")
+    public String getEnvDetails() {
+        return environment.toString();
     }
 
     public GreetingController() {
